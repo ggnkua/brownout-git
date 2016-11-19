3234d8169e6649880bcf66cfaabca0460f90ba48
ggn <ggn@atari.org>
2016-11-19 13:31:56 +0200

    Let's move the source code at the root folder, shall we not? I hate digging through endless folders just to get to one or two source files.
    Also, let's be hip and add a readme.

--- brownout.cpp
+++ brownout.cpp
@@ -1,25 +1,16 @@
 /*
-elfdump.cpp - Dump ELF file using ELFIO library.
 
-Copyright (C) 2001-2015 by Serge Lamikhov-Center
+brownout - A humble .elf to ST .prg binary converter
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
+Written by George Nakos and Douglas Litte.
 
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
+Uses the elfio library by Serge Lamikhov-Center to do the
+heavy lifting that is ELF parsing. See elfio.hpp for its
+license.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
+Everything apart from elfio library is released under
+the WTFPL. Probably.
+
 */
 
 #ifdef _MSC_VER
@@ -249,8 +240,8 @@ int main( int argc, char** argv )
                     }
                 default:
                     {
-                        std::cout << "What the hell kind of type that? "
-                            << (int)type << "? Really?" << std::endl;
+                        //std::cout << "What the hell kind of type that? "
+                        //    << (int)type << "? Really?" << std::endl;
                         break;
                     }
                 }
---
+++ readme.md
@@ -0,0 +1,11 @@
+brownout - A humble .elf to ST .prg binary converter
+
+Written by George Nakos and Douglas Litte.
+
+Uses the elfio library by Serge Lamikhov-Center to do the
+heavy lifting that is ELF parsing. See elfio.hpp for its
+license.
+
+Everything apart from elfio library is released under
+the WTFPL. Probably.
+
